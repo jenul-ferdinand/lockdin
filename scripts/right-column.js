bd0008ef982cf101c2780290b407d5e719b35eab
Jenul Ferdinand <jenul15ferdinand@gmail.com>
2024-03-02 18:56:16 +1100

    Fixed todo list page problem and some code commenting  and cleaning

--- scripts/right-column.js
+++ scripts/right-column.js
@@ -1,42 +1,58 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const dots = document.querySelectorAll('.dot');
-  dots.forEach((dot, index) => {
-    dot.addEventListener('click', () => moveToPage(index + 1));
-  });
+// Current page
+let currentPage = 1
 
-  moveToPage(currentPage); // Initialize
+
+
+// EVENT LISTENER FUNCTION: When the DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+	// Store all dots class html elements
+	const dots = document.querySelectorAll('.dot');
+
+	// For every dor assign an event listener to go to their respective page
+	dots.forEach((dot, index) => {
+		dot.addEventListener('click', () => moveToPage(index + 1));
+	});
+
+	// Move to the first page
+	moveToPage(currentPage); // Initialize
 });
 
 
-let currentPage = 1
 
+// FUNCTION: Moving to pages based on the page number
 function moveToPage(pageNumber) {
-  currentPage = pageNumber;
-  const pages = document.querySelectorAll('.page');
+	// Set the current page to the page number we want to go to
+	currentPage = pageNumber;
 
-  // Hide all pages
-  pages.forEach((page) => {
-    page.style.display = 'none';
-  });
+	// Store all page class elements 
+	const pages = document.querySelectorAll('.page');
 
-  // Show the selected page
-  const selectedPage = document.getElementById(`page${currentPage}`);
-  selectedPage.style.display = 'block';
+	// Hide all pages
+	pages.forEach((page) => {
+		page.style.display = 'none';
+	});
 
-  updatePaginationDots();
+	// Show the selected page
+	const selectedPage = document.getElementById(`page${currentPage}`);
+	selectedPage.style.display = 'block';
+
+	// Update the current dot
+	updatePaginationDots();
 }
 
+
+
+// FUNCTION: For changing the dots at the bottom
 function updatePaginationDots() {
-  const dots = document.querySelectorAll('.dot');
-  dots.forEach((dot, index) => {
-    if (index + 1 === currentPage) {
-      dot.classList.add('active');
-    } else {
-      dot.classList.remove('active');
-    }
-  });
+	const dots = document.querySelectorAll('.dot');
+	dots.forEach((dot, index) => {
+	if (index + 1 === currentPage) {
+		dot.classList.add('active');
+	} else {
+		dot.classList.remove('active');
+	}
+	});
 }
-
 
 
 
