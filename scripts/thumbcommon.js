6ac5e1283fc7505b6c09c68ebf43b78cb1966650
louisf412 <126870649+louisf412@users.noreply.github.com>
2024-03-02 14:06:03 +1100

    Added youtube thumbnail blurring

---
+++ scripts/thumbcommon.js
@@ -0,0 +1,35 @@
+//Define chrome browser in error/bug case
+if (typeof globalThis.browser === "undefined") {
+    globalThis.browser = globalThis.chrome;
+  }
+
+  /**
+ * @typedef {{
+ *   syncSettings: boolean,
+  *   thumbnailMode: 'blurred' | 'normal',
+  * }} Options
+  */
+
+ /** @type {Options} */
+const defaultOptions = {
+    syncSettings: true,
+    thumbnailMode: 'blurred',
+  }
+
+
+  /**
+ * @returns {Promise<Options>}
+ */
+const loadOptions = async () => {
+    /** @type {boolean} */
+    const syncSettings = (await new Promise((resolve) => {
+      browser.storage.local.get(defaultOptions, resolve);
+    })).syncSettings ?? defaultOptions.syncSettings;
+  
+    const options = await new Promise((resolve) => {
+      browser.storage[syncSettings ? 'sync' : 'local'].get(defaultOptions, resolve);
+    })
+  
+    return { ...defaultOptions, ...options }
+  }
+  
